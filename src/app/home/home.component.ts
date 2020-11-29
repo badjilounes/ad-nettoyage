c8836e28d65deeb5f40e1df9b57c46bc697b35b1
Badji Lounes <lounes.badji@cpe.fr>
2020-11-29 18:44:45 +0100

    image changed + mobile design for small screens

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,10 @@ import { BreakpointService } from '../core/breakpoint/breakpoint.service';
 export class HomeComponent implements OnInit {
 
   slides = [
-    'https://www.societe-nettoyage-lyon.com/wp-content/uploads/2017/09/cleaning-268112_1920.jpg',
-    'https://www.societe-nettoyage-lyon.com/wp-content/uploads/2019/03/Lyon-Ville-Propre.jpg',
-    'https://www.societe-nettoyage-lyon.com/wp-content/uploads/2017/09/Slide-3-JPG.jpg',
-    'https://www.societe-nettoyage-lyon.com/wp-content/uploads/2017/10/Nettoyage-Lyon-Entreprise-Societe-2-1.jpg',
+    'assets/images/home/nettoyage-1.jpg',
+    'assets/images/home/nettoyage-2.jpg',
+    'assets/images/home/nettoyage-3.jpg',
+    'assets/images/home/nettoyage-4.jpg',
   ];
 
   isHandset$: Observable<boolean> = this.breakpoint.isHandset$;
