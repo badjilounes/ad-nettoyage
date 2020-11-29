7e225414f3e5540b4dc3cf5b3a1066d54f0a524a
Badji Lounes <lounes.badji@cpe.fr>
2020-11-29 02:30:34 +0100

    fix: wrong server root

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 
-const root = 'ad-nettoyage';
+const root = 'dist/ad-nettoyage';
 const app = express();
 
 app.use(forceHttpsIfProxy);
